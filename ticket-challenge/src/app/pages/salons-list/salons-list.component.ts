d72332cdb9768903ef33b8929a5d0507f5ec32c3
amir fatemi <amirhosseinfatemi559@gmail.com>
2023-11-10 18:18:33 +0330

    [feat] salons list data fetch template  and create core modules

--- ticket-challenge/src/app/pages/salons-list/salons-list.component.ts
+++ ticket-challenge/src/app/pages/salons-list/salons-list.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { MapService } from 'src/app/core/services/map/map.service';
 
 @Component({
-  selector: 'app-salons-list',
-  templateUrl: './salons-list.component.html',
-  styleUrls: ['./salons-list.component.scss']
+    selector: 'app-salons-list',
+    templateUrl: './salons-list.component.html',
+    styleUrls: ['./salons-list.component.scss'],
 })
 export class SalonsListComponent implements OnInit {
+    salonsList: string[] = [];
+    constructor(private mapService: MapService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    ngOnInit(): void {
+        this.getAllSalons();
+    }
+    getAllSalons() {
+        this.mapService.getAll().subscribe((res) => {
+            this.salonsList = res;
+        });
+    }
+    trackById(index: number, salons: any) {
+        return index;
+    }
 }
